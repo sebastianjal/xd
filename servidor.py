a25070eb8b11f611f91987648d34192cc8e96cde
sebastianjal <sebasjarel9@gmail.com>
2024-05-16 21:22:24 +0000

    Mundo guardado 2024-05-16

--- servidor.py
+++ servidor.py
@@ -1,11 +1,12 @@
 import os
 import requests
 import base64
-import schedule  # type: ignore
+import schedule
 import time
 from flask import Flask
+from threading import Thread
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
@@ -15,21 +16,21 @@ def pressspace():
     print("Simulación de presionar espacio")
     # Aquí puedes agregar cualquier otra funcionalidad que necesites
 
-def downloadlatestrelease(downloadpath='.'):
+def download_latest_release(download_path='.'):
     mirror = "https://elyxdev.github.io/latest"
     pet = requests.get(mirror)
     if pet.status_code == 200:
         data = pet.json()
         url = data.get('url')
         version = url.split("/")[-1]
-        pathto = os.path.join(download_path, version)
-        with open(pathto, 'wb') as archivo:
+        path_to = os.path.join(download_path, version)
+        with open(path_to, 'wb') as archivo:
             archivo.write(requests.get(url).content)
         return version
 
 def main():
     # Programar la ejecución de press_space() cada 3 horas
-    schedule.every(3).hours.do(press_space)
+    schedule.every(3).hours.do(pressspace)
 
     if not os.path.exists("./.gitignore"):
         big = "L3dvcmtfYXJlYQ0KL3NlcnZpZG9yX21pbmVjcmFmdA0KL21pbmVjcmFmdF9zZXJ2ZXINCi9zZXJ2aWRvcl9taW5lY3JhZnRfb2xkDQovdGFpbHNjYWxlLWNzDQovdGhhbm9zDQovYmtkaXINCi92ZW5kb3INCmNvbXBvc2VyLioNCmNvbmZpZ3VyYXRpb24uanNvbg0KY29uZmlndXJhY2lvbi5qc29uDQoqLnR4dA0KKi5weWMNCioub3V0cHV0"
@@ -47,11 +48,10 @@ def main():
         schedule.run_pending()
         time.sleep(1)
 
-if __name == "__main":
+if __name__ == "__main__":
     # Agregar una pausa para permitir copiar la URL
     print("Esperando 60 segundos para copiar la URL de Codespaces...")
     time.sleep(60)
-
     codespace_name = os.environ.get('CODESPACE_NAME')
     if codespace_name:
         codespace_url = f"https://{codespace_name}-8080.githubpreview.dev/"
@@ -60,7 +60,6 @@ if __name == "__main":
         print("No se pudo determinar la URL del Codespace.")
 
     # Iniciar el servidor Flask en un hilo separado
-    from threading import Thread
     server_thread = Thread(target=lambda: app.run(host='0.0.0.0', port=8080))
     server_thread.start()
 
